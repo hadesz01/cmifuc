7c32427cf7e9d60654aa9b0b5625254bfd58a3f1
Quang Nguyen <thanhquang.97bg@gmail.com>
2018-06-03 22:41:54 +0700

    v12

--- BTLHCGChuanDoanHongHocMayTinh/BTLHCGChuanDoanHongHocMayTinh/BTLHCG.Designer.cs
+++ BTLHCGChuanDoanHongHocMayTinh/BTLHCGChuanDoanHongHocMayTinh/BTLHCG.Designer.cs
@@ -52,10 +52,10 @@
             this.lblLoi = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.lblKetQua = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -289,34 +289,26 @@
             this.groupBox3.Controls.Add(this.lblKetQua);
             this.groupBox3.Location = new System.Drawing.Point(12, 302);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(760, 146);
+            this.groupBox3.Size = new System.Drawing.Size(760, 175);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             // 
             // lblKetQua
             // 
+            this.lblKetQua.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblKetQua.Location = new System.Drawing.Point(128, 26);
+            this.lblKetQua.Location = new System.Drawing.Point(116, 49);
             this.lblKetQua.Name = "lblKetQua";
             this.lblKetQua.Size = new System.Drawing.Size(556, 65);
             this.lblKetQua.TabIndex = 0;
             this.lblKetQua.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label6
-            // 
-            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 479);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(212, 13);
-            this.label6.TabIndex = 3;
-            this.label6.Text = "Phầm mềm chuẩn đoán hỏng hóc máy tính";
-            // 
             // label7
             // 
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(667, 451);
+            this.label7.Location = new System.Drawing.Point(460, 483);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(61, 13);
             this.label7.TabIndex = 4;
@@ -326,7 +318,7 @@
             // 
             this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(641, 467);
+            this.label8.Location = new System.Drawing.Point(529, 483);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(105, 13);
             this.label8.TabIndex = 5;
@@ -336,11 +328,21 @@
             // 
             this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(641, 483);
+            this.label9.Location = new System.Drawing.Point(653, 483);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(113, 13);
             this.label9.TabIndex = 5;
             this.label9.Text = "Nguyễn Thành Quang";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(9, 480);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Phiên bản 1.0";
             // 
             // BTLHCG
             // 
@@ -349,10 +351,10 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(784, 501);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -394,11 +396,11 @@
         private System.Windows.Forms.Label lblLoiChiTiet;
         private System.Windows.Forms.ComboBox cboNutDieuKhien;
         private System.Windows.Forms.Label lblKetQua;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label10;
     }
 }
 
